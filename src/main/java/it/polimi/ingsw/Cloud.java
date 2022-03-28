682dec79a1ce38ea96f14f246c1020340ef70701
eliskina <elis.kina@mail.polimi.it>
2022-03-28 17:28:52 +0200

    Island class

--- src/main/java/it/polimi/ingsw/Cloud.java
+++ src/main/java/it/polimi/ingsw/Cloud.java
@@ -4,6 +4,9 @@ public class Cloud {
 
     private Student[] students;
 
+    // if(Player1, Player2) {students = new Student[3]}
+    // if(Player1, Player2, Player 3) {students = new Student[4]}
+
     /**
      * Default constructor
      *
---
+++ src/main/java/it/polimi/ingsw/Island.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw;
+
+public class Island {
+
+    private int numIslands = 12;
+
+
+    // possiamo creare una List<Island> che ad ogni isola possiamo associare un valore boolean per indicare se c'è o no
+    // motherNature, un int(oppure 5 siccome ci sono 5 professori) che ci da il numero degli studenti di ogni colore e
+    // un int per indicare se c'è o no un Tower.
+    // Facendo così possiamo sommare il Tower con gli studenti per poi calcolare la supremazia.
+}
