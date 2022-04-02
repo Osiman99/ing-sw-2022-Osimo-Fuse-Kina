d8683b41d5f7c529cf7570bee3cfe97c5cfa68ab
eliskina <elis.kina@mail.polimi.it>
2022-04-02 18:19:09 +0200

    empty list exception

--- src/main/java/it/polimi/ingsw/model/BagSingleton.java
+++ src/main/java/it/polimi/ingsw/model/BagSingleton.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class BagSingleton {
 
+    private Game game;
     private static List<Student> students;
 
     /**
@@ -31,7 +32,28 @@ public class BagSingleton {
         return instance;
     }
 
-    public void moveStudent(){
 
+
+    /**
+     * if there are 2 players Cloud gets 3 students from the Bag each round,
+     * if there are 3 players it gets 4.
+     */
+    public void moveStudentToCloud() {
+
+       try {
+           if (this.game.getNumPlayers() == 2){
+               for(int i = 0; i < 3; i++){
+                   students.remove(0);
+               }
+           }else if (this.game.getNumPlayers() == 3){
+               for(int i = 0; i < 4; i++){
+                   students.remove(0);
+               }
+           }
+       }
+       catch ( EmptyListException emptyListException){
+           //..........
+       }
     }
+
 }
