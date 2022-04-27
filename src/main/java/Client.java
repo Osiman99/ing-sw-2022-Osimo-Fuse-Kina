3e7c5148f2bfed2caf506f9f6c33a869be066003
eliskina <elis.kina@mail.polimi.it>
2022-04-27 20:04:01 +0200

    server&client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,7 +1,78 @@
-public class Client implements Runnable{
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
-    @Override
-    public void run() {
+public class Client{
+
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    public static void main(String[] args) {
+        new Client();
+    }
+
+    public Client(){
+        try {
+            Socket socket = new Socket("localhost", 3333);
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            listenForInput();
+        }catch (UnknownHostException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void listenForInput(){
+        Scanner console = new Scanner(System.in);
+
+        while (true){
+            while(console.hasNextLine()) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            String input = console.nextLine();
+
+            if (input.toLowerCase().equals("quit")){
+                break;
+            }
+
+            try {
+                dataOutputStream.writeUTF(input);
+
+                while (dataInputStream.available() == 0){
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                String reply = dataInputStream.readUTF();
+                System.out.println(reply);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+        try {
+            dataInputStream.close();
+            dataOutputStream.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
---
+++ src/main/java/Server.java
@@ -0,0 +1,63 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    ServerSocket serverSocket;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    public static void main(String[] args) {
+
+        new Server();
+    }
+
+    public Server(){
+        try {
+            serverSocket = new ServerSocket(3333);
+            socket = serverSocket.accept();
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            listenForData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listenForData(){
+        while(true) {
+            try {
+                while (dataInputStream.available() == 0) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                String dataIn = dataInputStream.readUTF();
+
+                dataOutputStream.writeUTF(dataIn);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+        try {
+            dataInputStream.close();
+            dataOutputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
