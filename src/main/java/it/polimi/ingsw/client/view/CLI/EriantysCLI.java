0b36f1adb8f33165155e0213d2d2697f28002f82
eliskina <elis.kina@mail.polimi.it>
2022-05-09 19:42:39 +0200

    CLI

--- src/main/java/it/polimi/ingsw/client/view/CLI/EriantysCLI.java
+++ src/main/java/it/polimi/ingsw/client/view/CLI/EriantysCLI.java
@@ -1,19 +1,56 @@
 package it.polimi.ingsw.client.view.CLI;
 
+import java.util.ArrayList;
+
 public class EriantysCLI {
 
     public static String serverHostName;
     public static int serverPortNumber;
 
 
+
+    public ArrayList<String> welcome(String idIsola){
+        ArrayList<String> res= new ArrayList<>();
+        res.add(ANSIColor.BLACK+ANSIColor.RED_BACKGROUND+"ISOLA"+idIsola);
+        res.add("+-----------------+");
+        res.add("|                 |"/*+ANSIColor.RESET*/);
+        res.add("|                 |");
+        res.add("|                 |");
+        res.add("+-----------------+");
+        return res;
+    }
+
+
+
     public static void main(String[] args) {
-        if(args.length < 2) {
+        /*if(args.length < 2) {
             System.out.println("Please provide server address and port");
             return;
         }
 
         serverHostName = args[0];
         serverPortNumber = Integer.parseInt(args[1]);
+        */
+
+        EriantysCLI cli = new EriantysCLI();
+        ArrayList<String> esempio = new ArrayList<>();
+/*
+        esempio.add("WELCOME");
+        esempio.add("TO");
+        esempio.add("ERIANTYS");
+        for(String s:esempio)
+            System.out.println(s);
+*/
+/*
+        System.out.println("WELCOME" +
+                "\n" +
+                "TO" +
+                "\n" +
+                "ERIANTYS");*/
+
+        esempio = cli.welcome("5");
+        for(String s:esempio)
+            System.out.println(s);
 
     }
 }
--- src/main/java/it/polimi/ingsw/client/view/CLI/Welcome.java
+++ src/main/java/it/polimi/ingsw/client/view/CLI/Welcome.java
@@ -3,12 +3,20 @@ package it.polimi.ingsw.client.view.CLI;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-@Command(
-        name = "WELCOME",
-        description = "it prints WELCOME"
-)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Command(name = "WELCOME")
 
 public class Welcome implements Runnable{
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public Welcome(){
+        //reader =  new BufferedReader(new InputStreamReader(System.in));
+    }
+
 
     public static void main(String[] args) {
         new CommandLine(new Welcome()).execute(args);
@@ -17,5 +25,19 @@ public class Welcome implements Runnable{
     @Override
     public void run() {
         System.out.println("WELCOME");
+        System.out.println("Insert nickname");
+        do{
+            try {
+                String s = reader.readLine();
+                //controller.send(s);
+                if(s.equals("elis")){
+                    System.out.println("nickname accepted");
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }while(true);
+
     }
 }
