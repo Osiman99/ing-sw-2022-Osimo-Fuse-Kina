fed40d576ba1358f6bfaceb4599a2b1304275e55
eliskina <elis.kina@mail.polimi.it>
2022-05-12 12:56:11 +0200

    adding some more network messages

---
+++ src/main/java/it/polimi/ingsw/network/messages/LobbyMessage.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.network.messages;
+
+import it.polimi.ingsw.server.model.Game;
+
+import java.util.List;
+
+/**
+ * Message which contains the current players and the maximum size of the lobby .
+ */
+public class LobbyMessage extends Message{
+    private static final long serialVersionUID = -6886305903361404798L;
+    private final List<String> nicknameList;
+    private final int maxPlayers;
+
+    public LobbyMessage(List<String> nicknameList, int maxPlayers){
+        super(Game.SERVER_NICKNAME, MessageType.LOBBY);
+        this.nicknameList = nicknameList;
+        this.maxPlayers = maxPlayers;
+    }
+
+    public List<String> getNicknameList(){
+        return nicknameList;
+    }
+
+    public int getMaxPlayers(){
+        return maxPlayers;
+    }
+
+
+    @Override
+    public String toString() {
+        return "LobbyMessage{" +
+                "nickname=" + getNickname() +
+                ", nicknameList=" + nicknameList +
+                ", numPlayers=" + maxPlayers +
+                '}';
+    }
+}
