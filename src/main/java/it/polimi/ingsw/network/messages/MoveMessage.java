dacce1e22c7e0878e0aa172ed93a139ae3632a71
Osiman99 <davideosimo@gmail.com>
2022-05-25 14:13:25 +0200

    moveStudent

---
+++ src/main/java/it/polimi/ingsw/network/messages/MoveMessage.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.network.messages;
+
+import it.polimi.ingsw.server.model.StudentColor;
+
+public class MoveMessage extends Message {
+    private static final long serialVersionUID = -7726935391031946027L;
+    private final StudentColor studentColor;
+    private final int numIsland;
+
+
+    public MoveMessage(String nickname, StudentColor studentColor, int numIsland) {
+        super(nickname, MessageType.MOVE_STUDENT);
+        this.studentColor = studentColor;
+        this.numIsland = numIsland;
+    }
+
+    public StudentColor getStudentColor() {
+        return studentColor;
+    }
+
+    public int getNumIsland() {
+        return numIsland;
+    }
+
+    public String toString() {
+        return "Chosen student to move:" + studentColor;
+    }
+}
