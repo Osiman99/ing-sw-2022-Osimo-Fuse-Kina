022be8394a00c45de2a15d801077fdbfdf85ad67
eliskina <elis.kina@mail.polimi.it>
2022-05-30 22:29:01 +0200

    setResizable -> false

--- src/main/java/it/polimi/ingsw/client/view/GUI/Main.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI/Main.java
@@ -27,7 +27,7 @@ public class Main extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.setFullScreen(false);
-        primaryStage.setResizable(true);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 }
