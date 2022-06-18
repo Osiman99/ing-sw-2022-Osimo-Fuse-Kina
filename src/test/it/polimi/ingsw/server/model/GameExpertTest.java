b904e6da5fb9281c475d90acc7eaaa8f7ef6bf2c
zRiise <fuse.ale123@gmail.com>
2022-06-18 12:22:54 +0200

    testing e modifiche

--- src/test/it/polimi/ingsw/server/model/GameExpertTest.java
+++ src/test/it/polimi/ingsw/server/model/GameExpertTest.java
@@ -8,13 +8,13 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameExpertTest {
-    private static GameExpert gameExpert;
-    private static Game game;
+
+    private static GameExpert game;
     private static List<String> nicknames = List.of(new String[]{"AlanTuring", "JamesGosling", "GeorgeBoole"});
 
     @BeforeAll
     static void setUp() {
-        game = Game.getInstance();
+        game = new GameExpert();
         for(int i=0; i<3; i++)
             game.getPlayers().add(new Player(nicknames.get(i)));
         game.setChosenPlayersNumber(3);
@@ -22,12 +22,12 @@ class GameExpertTest {
         game.getPlayers().get(1).setPlayerColor(TowerColor.WHITE);
         game.getPlayers().get(2).setPlayerColor(TowerColor.GREY);
         game.initGame();
+        game.initGameExpert();
 
-        gameExpert = new GameExpert();
     }
 
     @Test
     void getThreeChosenCards() {
-        assertEquals(3, gameExpert.getThreeChosenCards().size());
+        assertEquals(3, game.getThreeChosenCards().size());
     }
 }
